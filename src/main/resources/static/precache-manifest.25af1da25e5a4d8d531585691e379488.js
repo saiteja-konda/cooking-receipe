98e251d828c69011c3bc3367cdd33663622c9473
saiteja <kondasaitej@protonmail.com>
2020-09-24 12:17:51 +0530

    test build deploy with clever cloude

---
+++ src/main/resources/static/precache-manifest.25af1da25e5a4d8d531585691e379488.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b579660ef3515d952159e86adfc9d724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4497b96b16f38ebfb43",
+    "url": "/static/css/2.658ca8a9.chunk.css"
+  },
+  {
+    "revision": "aed2f9b1dcd517c867de",
+    "url": "/static/css/main.701105bc.chunk.css"
+  },
+  {
+    "revision": "e4497b96b16f38ebfb43",
+    "url": "/static/js/2.5e964b27.chunk.js"
+  },
+  {
+    "revision": "5ced2f2ce7b79285485a360b83230d1b",
+    "url": "/static/js/2.5e964b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed2f9b1dcd517c867de",
+    "url": "/static/js/main.5b08257c.chunk.js"
+  },
+  {
+    "revision": "71e0c3685804b4da1be7",
+    "url": "/static/js/runtime-main.c974dedc.js"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/static/media/ionicons.99b86349.woff"
+  }
+]);
