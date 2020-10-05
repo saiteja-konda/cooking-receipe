0243f32905b4140281b41ae307f92c3ec0c13f5b
saiteja <kondasaitej@protonmail.com>
2020-10-05 12:04:17 +0530

    bug fix

--- src/main/java/com/teja/blog/repository/CommentRepository.java
+++ src/main/java/com/teja/blog/repository/CommentRepository.java
@@ -2,9 +2,10 @@ package com.teja.blog.repository;
 
 import com.teja.blog.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
     Comment findByPostId(Long id);
--- src/main/java/com/teja/blog/repository/PostRepository.java
+++ src/main/java/com/teja/blog/repository/PostRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
+@Repository
 public interface PostRepository extends JpaRepository<Post,Long>, JpaSpecificationExecutor<Post> {
    List<Post> findAllByGenre(String genre);
    List<Post> findPostsByTypeOrderByPostedOnAsc(String type);
